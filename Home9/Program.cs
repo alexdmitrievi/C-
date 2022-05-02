8166decf30aec0f8b283684962ad4adf8f382c24
alexdmitrievi <103923985+alexdmitrievi@users.noreply.github.com>
2022-05-02 20:08:29 +0600

    end

---
+++ Home9/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+long a = Convert.ToInt64(Console.ReadLine());
+if (a < 100) {
+    Console.WriteLine("третьей цифры нет");
+}
+else {
+    while (a > 1000) {
+        a = a/10;
+    }
+    a = a%10;
+    Console.WriteLine(a);
+}
