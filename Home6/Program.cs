e3aa631ebcb2dd8a510a0959b83a39973aa33b09
alexdmitrievi <103923985+alexdmitrievi@users.noreply.github.com>
2022-05-02 19:11:04 +0600

    ДЗ

---
+++ Home6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите двухзначное число"); 
+int a = Convert.ToInt32(Console.ReadLine());
+if (a%10 > a/10) {
+    Console.WriteLine(a%10);
+}
+else {
+    Console.WriteLine(a/10);
+}
