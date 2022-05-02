6d790e3611c6c176336b346cb0f48c154ce998dd
alexdmitrievi <103923985+alexdmitrievi@users.noreply.github.com>
2022-05-02 19:36:53 +0600

    home8

---
+++ Home8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите два числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a % b == 0)  {
+    Console.Write("да");
+}
+else {
+    Console.Write(a%b);
+}
